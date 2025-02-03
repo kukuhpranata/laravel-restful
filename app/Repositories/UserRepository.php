d6c4e7d87eb8029b8429291b0626a621fc02f55b
kukuhpranata <kukuhpranata000@gmail.com>
2025-02-03 21:44:48 +0700

    clean

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -7,27 +7,33 @@ use App\Interfaces\UserRepositoryInterface;
 
 class UserRepository implements UserRepositoryInterface
 {
-    public function index(){
-        return User::all();
-    }
+   public function index()
+   {
+      return User::all();
+   }
 
-    public function getById($id){
-       return User::findOrFail($id);
-    }
+   public function getById($id)
+   {
+      return User::find($id);
+   }
 
-    public function getByemail($email){
-       return User::where('email', $email)->first();
-    }
+   public function getByemail($email)
+   {
+      return User::where('email', $email)->first();
+   }
 
-    public function store(array $data){
-       return User::create($data);
-    }
+   public function store(array $data)
+   {
+      return User::create($data);
+   }
 
-    public function update(array $data,$id){
-       return User::whereId($id)->update($data);
-    }
-    
-    public function delete($id){
-       User::destroy($id);
-    }
+   public function update(array $data, $id)
+   {
+      return User::whereId($id)->update($data);
+   }
+
+   public function delete($id)
+   {
+      User::destroy($id);
+   }
 }
